8cabb25734fd4a46be42e9f0b60c076873fb8e6b
TonySantiagoRodrigues <betovem19@hotmail.com>
2022-12-10 22:21:35 -0300

    add js

---
+++ exercicio.js
@@ -0,0 +1,16 @@
+
+let naame = prompt("Informe o nome da pessoa mais velha.")
+
+let irmaomaisvelho = prompt ("informe a idade da pessoa mais velha")
+
+let naamee = prompt ("Informe o nome da pessoa mais nova.")
+
+let agee = prompt ("informe a idade da pessoa mais nova")
+
+let dif =   irmaomaisvelho  - agee
+
+alert("Nome do mais velho:" + naame + "\nIdade do mais velho: " + irmaomaisvelho)  
+
+alert("Nome do mais novo:" + naamee + "\nIdade do mais novo: "  + agee)
+
+alert("Diferença de idade: " + dif)
---
+++ test.js
@@ -0,0 +1,12 @@
+alert("Bem vindo! A seguir pediremos que informe alguns dados.")
+
+
+let naame = prompt("Informe seu Nome.")
+
+
+let age = prompt("informe sua idade")
+
+
+let AgeConfirm = confirm("Sua idade é " + age + " anos ")
+
+alert("Nome: " + naame + "\nIdade: " + age  + "\nIdade Confirmada: "+ AgeConfirm")
